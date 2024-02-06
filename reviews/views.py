2e08c51aaa9b2871a8b48912927f1d067327fab7
Farezii <fpcrezende@gmail.com>
2024-02-06 14:14:11 -0400

    Changed view from method to class

--- reviews/views.py
+++ reviews/views.py
@@ -1,29 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
+from django.views import View
 
 # Create your views here.
-def review(request):
-    if request.method == 'POST':
+#View based on classes
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+
+        return render(request, 'reviews/review.html', {
+            'form': form,
+        })
+        
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            # Saving a form to a model
-            # review = Review(
-            #     user_name=form.cleaned_data['user_name'],
-            #     review_text=form.cleaned_data['review_text'],
-            #     rating=form.cleaned_data['rating'],)
-            # review.save()
-            
-            # Saving a ModelForm
             form.save()
             return HttpResponseRedirect('/thank-you')
-    else:
-        form = ReviewForm()
+        return render(request, 'reviews/review.html', {
+            'form': form,
+        })
+# View based on methods
+# def review(request):
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST)
 
-    return render(request, 'reviews/review.html', {
-        'form': form,
-    })
+#         if form.is_valid():
+#             # Saving a form to a model
+#             # review = Review(
+#             #     user_name=form.cleaned_data['user_name'],
+#             #     review_text=form.cleaned_data['review_text'],
+#             #     rating=form.cleaned_data['rating'],)
+#             # review.save()
+            
+#             # Saving a ModelForm
+#             form.save()
+#             return HttpResponseRedirect('/thank-you')
+#     else:
+#         form = ReviewForm()
+
+#     return render(request, 'reviews/review.html', {
+#         'form': form,
+#     })
 
 
 def thank_you(request):
