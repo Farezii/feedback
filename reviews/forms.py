ac3187d85b66bae9dc9adcf2e2e413c6fdd893d2
Farezii <fpcrezende@gmail.com>
2024-02-06 13:29:32 -0400

    added more forms, added single form div

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,4 +2,11 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(max_length=100)
+    user_name = forms.CharField(max_length=100, label='Your name', error_messages={
+        'required': 'Name field must not be empty',
+        'max_length': 'Name is too long'
+    })
+
+    review_text = forms.CharField(
+        label='Your feedback', max_length=200, widget=forms.Textarea)
+    rating = forms.IntegerField(label='Your rating', min_value=1, max_value=5)
